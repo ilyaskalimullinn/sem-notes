c8048effdff1df091f6991608560dca494aeef85
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-05-18 01:11:53 +0300

    build: add vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,12 @@
 <template>
-    <h1>Notes project</h1>
-    <h2>{{this.test}}</h2>
+  <router-view />
 </template>
 
 <style scoped>
 </style>
 
 <script>
-import {testApi} from "./services/api.js";
-
 export default {
-    data() {
-        return {
-            test: "Api not working"
-        }
-    },
-    async mounted() {
-        this.test = await testApi()
-    }
+  name: "App"
 }
 </script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Login from "../views/Login.vue";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+export default router
