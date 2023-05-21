815383847b8215e2e57393c69ed22228e81fff9a
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-05-21 17:04:09 +0300

    edit: NoteSerializer structure

--- backend/src/main/java/org/example/ilyaskalimullinn/notes/data/serializer/note/NoteSerializer.java
+++ backend/src/main/java/org/example/ilyaskalimullinn/notes/data/serializer/note/NoteSerializer.java
@@ -2,14 +2,10 @@ package org.example.ilyaskalimullinn.notes.data.serializer.note;
 
 import lombok.Builder;
 import lombok.Data;
-import org.example.ilyaskalimullinn.notes.data.serializer.note.block.NoteBlockSerializer;
-
-import java.util.List;
 
 @Data
 @Builder
 public class NoteSerializer {
-    private String version;
-    private Long time;
-    private List<NoteBlockSerializer> blocks;
+    private String title;
+    private NoteContentSerializer content;
 }
