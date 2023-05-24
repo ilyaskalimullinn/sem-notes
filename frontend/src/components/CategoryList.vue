8dd90992466ae3b4781c9ff7c83adc69ae6600b1
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-05-24 10:17:48 +0300

    feat: category list in front

---
+++ frontend/src/components/CategoryList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="category in categoryList" :key="category.id">
+      {{ category.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import {mapWritableState} from "pinia";
+import {useNoteStore} from "../stores/noteStore.js";
+
+export default {
+  name: "CategoryList",
+  computed: {
+    ...mapWritableState(useNoteStore, ["categoryList"])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
