49d1862dd20a19670d67ef80bf761f01381c6da1
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-05-24 18:22:09 +0300

    feat: styles notes list

--- frontend/src/components/CategoryList.vue
+++ frontend/src/components/CategoryList.vue
@@ -1,19 +1,24 @@
 <template>
-  <ul>
-    <li v-for="category in categoryList" :key="category.id">
-      {{ category.name }}
-    </li>
-  </ul>
+  <div class="form-check" v-for="category in categoryList">
+    <input class="form-check-input" type="checkbox"
+           :value="category.id"
+           v-model="activeCategoryList"
+           :id="category.id">
+    <label class="form-check-label" :for="category.id">
+      {{category.name}}
+    </label>
+  </div>
 </template>
 
 <script>
-import {mapWritableState} from "pinia";
+import {mapState, mapWritableState} from "pinia";
 import {useNoteStore} from "../stores/noteStore.js";
 
 export default {
   name: "CategoryList",
   computed: {
-    ...mapWritableState(useNoteStore, ["categoryList"])
+    ...mapState(useNoteStore, ['categoryList']),
+    ...mapWritableState(useNoteStore, ['activeCategoryList'])
   }
 }
 </script>
